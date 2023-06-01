a9e9402ad3036caa31242e11124c4153a4484c6d
Diogo Takamori Barbosa <42560225+Diogotb@users.noreply.github.com>
2023-06-01 01:51:43 +0000

    31/05

--- JavaScriptManipulacao/script.js
+++ JavaScriptManipulacao/script.js
@@ -1,43 +1,16 @@
-function cadPessoas(){
-    
-    let nome = document.getElementById("fNome").value;
-    let sobrenome = document.getElementById("fSobrenome").value;
-    let idade = document.getElementById("fIdade").value;
-    let email = document.getElementById("fEmail").value;
-    console.log(nome)
 
-    
-    let tb = document.getElementById("tabelaPessoas");
-    let qtdLinhas = tb.ariaRowSpan.length;
-    let linha = tb.insertRow(qtdLinhas);
-
-    let cellCodigo = linha.insertCell(0);
-    let cellNome = linha.insertCell(1);
-    let cellSobrenome = linha.insertCell(2);
-    let cellIdade = linha.insertCell(3);
-    let cellEmail = linha.insertCell(4);
-
-    cellCodigo.innerHTML = qtdLinhas;
-    cellNome.innerHTML = nome;
-    cellSobrenome.innerHTML = sobrenome;
-    cellIdade.innerHTML = idade;
-    cellEmail.innerHTML = email;
-    console.log(nome)
-}
-`<div class="card" style="width: 18rem;">
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item">nome</li>
-        <li class="list-group-item">sobrenome</li>
-        <li class="list-group-item">idade</li>
-    </ul>
-</div>`;
 function mudarConteudo() {
-    let nome = document.getElementById("fNome").value;
+    let nome = fPessoas.fNome.value;
     let sobrenome = document.getElementById("fSobrenome").value;
     let idade = document.getElementById("fIdade").value;
     let email = document.getElementById("fEmail").value;
+    let img = document.querySelector("#fAvatar");
+    
+    
+    let imagem = img.getAttribute('src');
 
     let card = `<div class="card" style="width: 18rem;">
+    <img src="${imagem}" class="card-img-top" alt="...">
     <ul class="list-group list-group-flush">
         <li class="list-group-item">${nome}</li>
         <li class="list-group-item">${sobrenome}</li>
@@ -47,4 +20,5 @@ function mudarConteudo() {
 </div>`;
 
     document.getElementById("paragrafo").innerHTML += card;
+    fPessoas.reset();
   }
