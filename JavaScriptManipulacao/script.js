b389a39e1d659be72e0d73a2ca7f22d41d7dc6ea
Diogotb <digogo@gmail.com>
2023-06-01 22:04:25 -0300

    01/06

--- JavaScriptManipulacao/script.js
+++ JavaScriptManipulacao/script.js
@@ -1,16 +1,34 @@
+function previewFile() {
+    let preview = document.querySelector('img');
+    let file    = document.querySelector('#fAvatar').files[0];
+    let reader  = new FileReader();
+  
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      
+    }
+  }
+
 
 function mudarConteudo() {
     let nome = fPessoas.fNome.value;
     let sobrenome = document.getElementById("fSobrenome").value;
     let idade = document.getElementById("fIdade").value;
     let email = document.getElementById("fEmail").value;
-    let img = document.querySelector("#fAvatar");
+    let imagem    = document.querySelector('#fAvatar').files[0];
+    let reader  = new FileReader();
+    let preview = document.querySelector('img');
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
     
-    
-    let imagem = img.getAttribute('src');
 
-    let card = `<div class="card" style="width: 18rem;">
-    <img src="${imagem}" class="card-img-top" alt="...">
+    let card = `<div class="card" style="width: 18rem; heigth=27rem">
+    <img src="${preview.src}" style="heigth: 50%" class="card-img-top" alt="...">
     <ul class="list-group list-group-flush">
         <li class="list-group-item">${nome}</li>
         <li class="list-group-item">${sobrenome}</li>
@@ -18,7 +36,7 @@ function mudarConteudo() {
         <li class="list-group-item">${email}</li>
     </ul>
 </div>`;
-
+    preview.src = "";
     document.getElementById("paragrafo").innerHTML += card;
     fPessoas.reset();
   }
